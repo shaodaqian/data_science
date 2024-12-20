b1eb5e16b47429c87996d362fc2c903126f8c227
Bill Shao <36480301+shaodaqian@users.noreply.github.com>
2024-12-20 08:08:14 +0000

    Update test.py

--- test.py
+++ test.py
@@ -171,3 +171,162 @@ test_y=y_scalar.inverse_transform(test_y.reshape(-1,1))
 print(pred.shape,test_y.shape)
 print('rmse', np.sqrt(mean_squared_error(test_y,pred)))
 print('mae', mean_absolute_error(test_y,pred))
+
+
+
+
+org_dataset=dataset.copy()
+
+def split_feet(row):
+    # nan check
+    if row != row :
+        return None
+    if "”" in row:
+        meter = row[row.rfind("”")+1:][:-2]
+        feet = round(float(row[:row.rfind("”")+1].split("’")[0]) + float(row[row.rfind("’")+1:row.rfind("”")])/12,2)
+    elif "’" in row:
+        meter = row[row.rfind("’")+1:][:-2]
+        feet = row[:row.rfind("’")]
+    return float(feet)
+
+def split_meter(row):
+    # nan check
+    if row is None:
+        return None
+    if row != row :
+        return None
+    if "”" in row:
+        meter = row[row.rfind("”")+1:][:-2]
+    elif "’" in row:
+        meter = row[row.rfind("’")+1:][:-2]
+    return float(meter)
+
+def split_price(row):
+    # nan check
+    if row == "N/A €" :
+        return None
+    return int(row[6: row.rfind("€")].replace(" ", ""))
+
+def split_ft2(row):
+    # nan check
+    if row != row :
+        return None
+    return float(row.split(" ft²")[0].strip())
+
+def split_m2(row):
+    # nan check
+    if row != row :
+        return None
+    return float(row.split("ft²")[-1].split("m²")[0].strip())
+
+def split_kg(row):
+    # nan check
+    if row != row :
+        return None
+    return float(row.split("lb")[-1].split("kg")[0].strip())
+
+def split_liters(row,lit):
+    if row != row :
+        return None
+    return float(row.split("gal")[-1].split(lit)[0].strip())
+
+def split_displacement(row):
+    if row != row :
+        return None
+    return float(row.split("ft²/T")[-1].split("m²/T")[0].strip())
+
+def split_kgm(row):
+    if row != row :
+        return None
+    return float(row.split("lb.ft")[-1].split("kg.m")[0].strip())
+
+def split_hp(row):
+    if row is None:
+        return None
+    if row != row :
+        return None
+    if len(row)==0:
+        return None
+    return float(row.split("HP")[0])
+
+def remove_about(row):
+    if row is None or row!=row:
+        return None
+    if 'About' in row:
+        return float(row.split("About")[-1])
+    else:
+        return float(row)
+
+
+# dataset["Engine(s) power [HP]"] = dataset["Engine(s) power"].apply(lambda x:int(x.split("HP")[0]) if x == x else None)
+# dataset.drop("Engine(s) power",axis=1,inplace=True)
+
+dataset["Number of hulls built"] = dataset["Number of hulls built"].apply(lambda x: remove_about(x))
+
+dataset["price [€]"] = dataset["Standard public price ex. VAT (indicative only)"].apply(lambda x: split_price(x))
+dataset.drop("Standard public price ex. VAT (indicative only)",axis=1,inplace=True)
+
+dataset["Critical hull speed [knots]"] = dataset["Critical hull speed"].apply(lambda x:float(x.split("knots")[0]) if x == x else None)
+dataset.drop("Critical hull speed",axis=1,inplace=True)
+
+dataset["Ballast ratio%"] = dataset["Ballast ratio"].apply(lambda x:float(x.split("%")[0]) if x == x else None)
+dataset.drop("Ballast ratio",axis=1,inplace=True)
+
+dataset["French customs tonnage"] = dataset["French customs tonnage"].apply(lambda x:float(x.split("Tx")[0]) if x == x else None)
+dataset.drop("French customs tonnage",axis=1,inplace=True)
+
+
+for column in ["Upwind sail area to displacement","Downwind sail area to displacement"]:
+    dataset[f"{column} [num]"] = dataset[column].apply(lambda x: split_displacement(x) )
+    dataset.drop(column,axis=1,inplace=True)
+    
+for column in ["Righting moment @ 1°","Righting moment @ 30°"]:
+    dataset[f"{column}"] = dataset[column].apply(lambda x: split_kgm(x) )
+    dataset.drop(column,axis=1,inplace=True)
+
+for column in dataset:
+    if "(min./max.)" in column:
+        dataset[column[:-11]+'min'] = dataset[column].apply(lambda x: x.split("/")[0].strip() if x == x else None)
+        dataset[column[:-11]+'max'] = dataset[column].apply(lambda x: x.split("/")[1].strip() if x == x else None)
+        # print(dataset[column[:-11]+'min'])
+        # for index, value in dataset[column[:-11]+'min'].items():
+        #     if value is not None and len(value)>0:
+        #         print(value)
+        #         print(float(value))
+        try:
+            dataset[column[:-11]+'min']= dataset[column[:-11]+'min'].apply(lambda x: float(x) if x is not None and len(x)>0 else None)
+            dataset[column[:-11]+'max']= dataset[column[:-11]+'max'].apply(lambda x: float(x) if x is not None and len(x)>0 else None)
+        except:
+            print(column,'not int')
+        dataset.drop(column,axis=1,inplace=True)
+
+
+for column in dataset:
+    mask=dataset[column].notna()
+    # print(dataset[column][mask])
+    # print(dataset[column][mask].iloc[0])
+    d=dataset[column][mask].iloc[0]
+    if isinstance(d, str) and column not in ["Chart table","Berth width (head/elbows/knees/feet)"]:
+        # if "(min./max.)" in column:
+        if column=="Berth width (head/feet)":
+            continue
+        if "m"==d[-1]:
+            # print('meter', column)
+            dataset[f"{column} [meter]"] = dataset[column].apply(lambda x: split_meter(x) )
+            dataset.drop(column,axis=1,inplace=True)
+        if "m²"==d[-2:]:
+            # print('meter square',column)
+            dataset[f"{column} [m²]"] = dataset[column].apply(lambda x: split_m2(x) )    
+            dataset.drop(column,axis=1,inplace=True)
+        if "liters"==d[-6:] or "Liters"==d[-6:]:
+            print('liters',column)
+            dataset[f"{column} [l]"] = dataset[column].apply(lambda x: split_liters(x,d[-6:]) )
+            dataset.drop(column,axis=1,inplace=True)
+        if "kg" == d[-2:]:
+            # print('kg',column)
+            dataset[f"{column} [kg]"] = dataset[column].apply(lambda x: split_kg(x) )
+            dataset.drop(column,axis=1,inplace=True)
+        if "HP" == d[-2:]:
+            # print('hp',column)
+            dataset[f"{column} [HP]"] = dataset[column].apply(lambda x: split_hp(x) )
+            dataset.drop(column,axis=1,inplace=True)
